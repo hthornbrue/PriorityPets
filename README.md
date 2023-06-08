d77fd2c3fd9ea958e67f525b755dd5f57843e3b4
Eliot Slavik <115732920+EliotSlavik@users.noreply.github.com>
2023-06-08 14:34:29 -0500

    Bootstrap and date and time picker added

--- README.md
+++ README.md
@@ -2,9 +2,20 @@ Install this stuff
 
 Back-End: npm install express mongodb mongoose
 
+
+<!-- Install for Front End -->
+
 # Date Picker
 
 npm install moment react-datepicker
+
+# Date and Time picker
+
+npm install react-datetime-picker
+
+# Bootstrap
+
+npm install bootstrap
 
 ---
 
--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DateTimePicker from "react-datetime-picker";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -9,7 +10,7 @@ function App() {
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
   const [dueDate, setDueDate] = useState(null);
-  const [reminder, setReminder] = useState("");
+  const [reminder, setReminder] = useState(new Date());
 
   const handleAddTask = () => {
     const newTask = {
@@ -28,7 +29,7 @@ function App() {
     setDescription("");
     setPriority("");
     setDueDate(null);
-    setReminder("");
+    setReminder(new Date());
   };
 
   const handleDeleteTask = (index) => {
@@ -43,57 +44,102 @@ function App() {
     setTasks(updatedTasks);
   };
 
+  useEffect(() => {
+    tasks.forEach((task) => {
+      if (!task.completed && task.reminder && task.dueDate) {
+        const now = moment();
+        const reminderTime = moment(task.dueDate).subtract(
+          moment(task.reminder).hours(),
+          "hours"
+        ).subtract(
+          moment(task.reminder).minutes(),
+          "minutes"
+        );
+
+        if (now.isSameOrAfter(reminderTime)) {
+          setTimeout(() => {
+            alert(`Reminder: ${task.title}`);
+          }, reminderTime.diff(now));
+        }
+      }
+    });
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <h1>Task Manager</h1>
-      <div>
+    <div className="container mt-5">
+      <h1 className="mb-4">Task Manager</h1>
+      <div className="mb-3">
         <input
           type="text"
+          className="form-control mb-2"
           placeholder="Title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <textarea
+          className="form-control mb-2"
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
-        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <select
+          className="form-control mb-2"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+        >
           <option value="">Priority</option>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
           <option value="high">High</option>
         </select>
-        <DatePicker
-          selected={dueDate}
+        <DateTimePicker
+          className="form-control mb-2"
+          value={dueDate}
           onChange={(date) => setDueDate(date)}
-          placeholderText="Due Date"
+          placeholder="Due Date"
         />
-        <input
-          type="text"
-          placeholder="Reminder"
+        <DateTimePicker
+          className="form-control mb-2"
           value={reminder}
-          onChange={(e) => setReminder(e.target.value)}
+          onChange={(date) => setReminder(date)}
+          placeholder="Reminder"
         />
-        <button onClick={handleAddTask}>Add Task</button>
+        <button className="btn btn-primary" onClick={handleAddTask}>
+          Add Task
+        </button>
       </div>
       <div>
         {tasks.map((task, index) => (
-          <div key={index}>
-            <h2>{task.title}</h2>
-            {task.description && <p>{task.description}</p>}
-            <p>Priority: {task.priority}</p>
-            <p>
-              Due Date:{" "}
-              {task.dueDate ? moment(task.dueDate).format("YYYY-MM-DD") : "-"}
-            </p>
-            <p>Reminder: {task.reminder}</p>
-            {!task.completed && (
-              <button onClick={() => handleCompleteTask(index)}>
-                Mark as Completed
+          <div key={index} className="card mb-3">
+            <div className="card-body">
+              <h2 className="card-title">{task.title}</h2>
+              {task.description && (
+                <p className="card-text">{task.description}</p>
+              )}
+              <p className="card-text">Priority: {task.priority}</p>
+              <p className="card-text">
+                Due Date:{" "}
+                {task.dueDate ? moment(task.dueDate).format("YYYY-MM-DD HH:mm") : "-"}
+              </p>
+              <p className="card-text">
+                Reminder:{" "}
+                {task.reminder ? moment(task.reminder).format("YYYY-MM-DD HH:mm") : "-"}
+              </p>
+              {!task.completed && (
+                <button
+                  className="btn btn-success mr-2"
+                  onClick={() => handleCompleteTask(index)}
+                >
+                  Mark as Completed
+                </button>
+              )}
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDeleteTask(index)}
+              >
+                Delete Task
               </button>
-            )}
-            <button onClick={() => handleDeleteTask(index)}>Delete Task</button>
+            </div>
           </div>
         ))}
       </div>
