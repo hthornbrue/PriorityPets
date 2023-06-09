005e9b4cb8ba499298ffd8f84254b9df18beb6e1
Karl-Knapp <Karl-Knapp@outlook.com>
2023-06-09 16:53:53 -0500

    Actually works this time.

---
+++ packages/server/routes/pets.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// Define the pet routes
+
+module.exports = router;
---
+++ packages/server/routes/tasks.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// Define the task routes
+
+module.exports = router;
