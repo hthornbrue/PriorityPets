28c65bb53f9fb1ab65094d5d0a672f069e59a19c
Hayden Thornbrue <116924071+hthornbrue@users.noreply.github.com>
2023-06-11 19:14:36 -0500

    Update routes

--- packages/server/routes/pets.js
+++ packages/server/routes/pets.js
@@ -1,6 +1,41 @@
 const express = require("express");
+const { Pet } = require("../models");
+
 const router = express.Router();
 
-// Define the pet routes
+router.get('/pet/:id', async (request, response) => {
+    const id = request.params.id;
+
+    try {
+        const pet = await Pet.findById(id).exec();
+
+        if (pet) {
+            response.json(pet);
+        } else {
+            response.status(404).json({ error: 'Pet not found.' });
+        }
+    } catch (error) {
+        response.status(500).json({ error: 'An error occurred when finding the pet.' });
+    }
+});
+
+router.post('/pet/:id', async (request, response) => {
+    const id = request.params.id;
+    const { name, appearance, hungerLevel, healthLevel } = request.body;
+
+    try {
+        const updatedPet = await Pet.findByIdAndUpdate(id, { name, appearance, hungerLevel, healthLevel }, {
+            new: true,
+        }).exec();
+
+        if (updatedPet) {
+            response.json(updatedPet);
+        } else {
+            response.status(404).json({ error: 'Pet not found.' });
+        }
+    } catch (error) {
+        response.status(500).json({ error: 'An error occurred when updating the pet.' });
+    }
+});
 
 module.exports = router;
--- packages/server/routes/tasks.js
+++ packages/server/routes/tasks.js
@@ -1,6 +1,38 @@
 const express = require("express");
+
+const { Task } = require("../models");
+
 const router = express.Router();
 
-// Define the task routes
+router.get('/tasks', async (request, response) => {
+    try {
+        const tasks = await Task.find().exec();
+        response.json(tasks);
+    } catch (error) {
+        response.status(500).json({ error: 'An error occurred when retrieving tasks.' });
+    }
+});
+
+router.post('/tasks', async (request, response) => {
+    const { name, description, dueDate, priority, completed, category, user } = request.body;
+
+    const newTask = new Task({
+        name,
+        description,
+        dueDate,
+        priority,
+        completed,
+        category,
+        user
+    });
+
+    try {
+        await newTask.save();
+        response.json(newTask);
+    } catch (error) {
+        response.status(500).json({ error: 'An error occurred when creating the task.' });
+    }
+});
+
 
 module.exports = router;
