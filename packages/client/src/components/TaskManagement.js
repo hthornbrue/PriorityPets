055ea9c47608326174a149a164c1eeea46f4071c
Clifford Hunter <clifford.hunter@mykenzie.snhu.edu>
2023-06-13 13:23:45 -0400

    pushing front end changes

--- packages/client/src/components/TaskManagement.js
+++ packages/client/src/components/TaskManagement.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Modal } from "react-bootstrap";
 import "./TaskManagement.css";
 
+const bgColor = ["khaki", "orange", "orangered"];
 function TaskManagement() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
@@ -14,22 +15,7 @@ function TaskManagement() {
   const [dueDate, setDueDate] = useState(null);
   const [reminder, setReminder] = useState(new Date());
   const [show, setShow] = useState(false);
-
-  let taskColor;
-
-  switch (priority) {
-    case "high":
-      taskColor = "red";
-      break;
-    case "medium":
-      taskColor = "orange";
-      break;
-    case "low":
-      taskColor = "green";
-      break;
-    default:
-      taskColor = "lightgrey";
-  }
+  const [edit, setEdit] = useState(false);
 
   const openModal = (e) => {
     e.preventDefault();
@@ -79,7 +65,6 @@ function TaskManagement() {
         const reminderTime = moment(task.dueDate)
           .subtract(moment(task.reminder).hours(), "hours")
           .subtract(moment(task.reminder).minutes(), "minutes");
-
         if (now.isSameOrAfter(reminderTime)) {
           setTimeout(() => {
             alert(`Reminder: ${task.title}`);
@@ -91,131 +76,145 @@ function TaskManagement() {
 
   return (
     <>
-      <Button className="modalButton" onClick={openModal}>
-        Add Task
-      </Button>
-      <Modal className="modal" show={show} onHide={closeModal}>
-        <div
-          className="container mt-5"
-          style={{ backgroundColor: "darkgreen" }}
-        >
-          <h1 className="mb-4" style={{ color: "white" }}>
-            Task Manager
-          </h1>
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control mb-2"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <textarea
-              className="form-control mb-2"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <select
-              className="form-control mb-2"
-              value={priority}
-              onChange={(e) => setPriority(e.target.value)}
-            >
-              <option value="">Priority</option>
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-            </select>
-            <DateTimePicker
-              className="form-control mb-2"
-              value={dueDate}
-              onChange={(date) => setDueDate(date)}
-              placeholder="Due Date"
-            />
-            <DateTimePicker
-              className="form-control mb-2"
-              value={reminder}
-              onChange={(date) => setReminder(date)}
-              placeholder="Reminder"
-            />
-            <Button className="float-right mt-3" onClick={handleAddTask}>
-              Add Task
-            </Button>
-            <Button
-              className="float-right mt-3"
-              style={{ marginLeft: "20px" }}
-              onClick={closeModal}
-            >
-              Close
-            </Button>
+      <div className="div-card2">
+        <Button className="modalButton" onClick={openModal}>
+          Add Task
+        </Button>
+        <Modal className="modal" show={show} onHide={closeModal}>
+          <div
+            className="container mt-5"
+            style={{
+              backgroundColor: "darkgreen",
+              borderRadius: "20px",
+              marginBottom: "30px",
+            }}
+          >
+            <h1 className="mb-4" style={{ color: "white" }}>
+              Task Manager
+            </h1>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <textarea
+                className="form-control mb-2"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <select
+                className="form-control mb-2"
+                name="Priority"
+                onChange={(e) => setPriority(e.target.value)}
+              >
+                <option value="">Priority</option>
+                <option value={0}>Low</option>
+                <option value={1}>Medium</option>
+                <option value={2}>High</option>
+              </select>
+              <span className="dueDateSpan">Set Due Date</span>
+              <DateTimePicker
+                className="form-control mb-2 datePicker"
+                value={dueDate}
+                onChange={(date) => setDueDate(date)}
+                placeholder="Due Date"
+              />
+              <span className="dueDateSpan">Set Reminder</span>
+              <DateTimePicker
+                className="form-control mb-2 datePicker"
+                value={reminder}
+                onChange={(date) => setReminder(date)}
+                placeholder="Reminder"
+              />
+              <Button className="float-right mt-3" onClick={handleAddTask}>
+                Add Task
+              </Button>
+              <Button
+                className="float-right mt-3"
+                style={{ marginLeft: "20px" }}
+                onClick={closeModal}
+              >
+                Close
+              </Button>
+            </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
 
-      {tasks.map((task, index) => (
-        <div key={index} className="listDiv">
-          <ol>
-            <li
-              className="task"
-              style={{
-                backgroundColor: taskColor,
-                textDecoration: task.completed ? "line-through" : "none",
-              }}
-            >
-              <div className="holder">
-                {!task.completed && (
-                  <input
-                    type="checkbox"
-                    name="completed"
-                    className="completed"
-                    checked={task.completed}
-                    onChange={() => handleCompleteTask(index)}
-                  />
-                )}
-                <span
-                  style={{
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                    marginLeft: "5px",
-                  }}
-                >
-                  check complete
-                </span>
-                <h2 className="title">{task.title}</h2>
-                {task.description && (
-                  <div className="description main">{task.description}</div>
-                )}
-                <br></br>
-                <div className="main">
-                  <span className="priority">Priority: {task.priority}</span>
-                  <br></br>
-                  <span className="para">
-                    Due Date:{" "}
-                    {task.dueDate
-                      ? moment(task.dueDate).format("YYYY-MM-DD HH:mm")
-                      : "-"}
+        {tasks.map((task, index) => (
+          <div key={index} className="listDiv">
+            <ol>
+              <li
+                className="task"
+                style={{
+                  backgroundColor: bgColor[task.priority],
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                <div className="holder">
+                  {!task.completed && (
+                    <input
+                      type="checkbox"
+                      name="completed"
+                      className="completed"
+                      checked={task.completed}
+                      onChange={() => handleCompleteTask(index)}
+                    />
+                  )}
+                  <span
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "bold",
+                      marginLeft: "5px",
+                    }}
+                  >
+                    Complete
                   </span>
+                  <h2 className="title">{task.title}</h2>
+                  {task.description && (
+                    <div className="description main">{task.description}</div>
+                  )}
                   <br></br>
-                  <span className="para">
-                    Reminder:{" "}
-                    {task.reminder
-                      ? moment(task.reminder).format("YYYY-MM-DD HH:mm")
-                      : "-"}
-                  </span>
+                  <div className="main">
+                    {/* <span className="priority">Priority:  </span> */}
+                    <br></br>
+                    <span className="para">
+                      Due:{" "}
+                      {task.dueDate
+                        ? moment(task.dueDate).format("ddd MMM DD 'YY h:mm")
+                        : "-"}
+                    </span>
+                    <br></br>
+                    <span className="para">
+                      REM:{" "}
+                      {task.reminder
+                        ? moment(task.reminder).format("ddd MMM DD 'YY h:mm")
+                        : "-"}
+                    </span>
+                    <Button
+                      className="editButton"
+                      onClick={(e) => setEdit(true)}
+                    >
+                      Edit
+                    </Button>
+
+                    <Button
+                      className="btn btn-danger deleteButtonTask"
+                      style={{ marginLeft: "30px", border: "2px black solid" }}
+                      onClick={() => handleDeleteTask(index)}
+                    >
+                      Delete Task
+                    </Button>
+                  </div>
                 </div>
-
-                <button
-                  className="btn btn-danger deleteButtonTask"
-                  style={{ marginLeft: "20px" }}
-                  onClick={() => handleDeleteTask(index)}
-                >
-                  Delete Task
-                </button>
-              </div>
-            </li>
-          </ol>
-        </div>
-      ))}
+              </li>
+            </ol>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- packages/client/src/pages/TaskPage.js
+++ packages/client/src/pages/TaskPage.js
@@ -3,10 +3,8 @@ import TaskManagement from "../components/TaskManagement";
 
 function TaskPage() {
   return (
-    <div
-      style={{ backgroundColor: "darkgreen", width: "100vw", height: "100vh" }}
-    >
-      <h1 style={{ color: "lightgray", fontWeight: "bold", fontSize: "60px" }}>
+    <div style={{ backgroundColor: "green", width: "100vw", height: "100vh" }}>
+      <h1 style={{ color: "white", fontWeight: "bold", fontSize: "60px" }}>
         User's Tasks
       </h1>
       <TaskManagement />
