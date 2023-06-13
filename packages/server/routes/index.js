3d08e51ff83c19c56b52da29bf7c58d68709572f
Karl-Knapp <Karl-Knapp@outlook.com>
2023-06-12 19:28:28 -0500

    Fixed stuff.

--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import userRouter from "./users";
 import taskRouter from "./tasks";
 import petRouter from "./pets";
-const listEndpoints = require("express-list-endpoints");
+import { User } from "../models";
+import listEndpoints from "express-list-endpoints";
 
 const router = express.Router();
-const User = require("../models");
 
 // Define routes
 router.get("/", async (req, res) => {
@@ -17,4 +17,4 @@ router.use("/users", userRouter);
 router.use("/tasks", taskRouter);
 router.use("/pets", petRouter);
 
-module.exports = router;
+export default router;
