50b5b445d0cb0946f037dee3acda8f4ff227cefe
Clifford Hunter <clifford.hunter@mykenzie.snhu.edu>
2023-06-15 23:40:28 -0400

    Added GravePicker, petPage,PetPicker not complete

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import TaskPage from "./pages/TaskPage";
 import HomePage from "./pages/HomePage";
+import PetPage from "./pages/PetPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/PetPage" element={<PetPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/TaskPage" element={<TaskPage />} />
     </Routes>
