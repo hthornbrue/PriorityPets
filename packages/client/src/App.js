c602988a2728ce8c4e6a73b59fa7f3b1b3dabab5
Eliot Slavik <115732920+EliotSlavik@users.noreply.github.com>
2023-06-20 12:35:47 -0500

    non logged in can not see other pages

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,15 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import TaskPage from "./pages/TaskPage";
 import HomePage from "./pages/HomePage";
 import PetPage from "./pages/PetPage";
+import { RequireAuth } from "react-auth-kit";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/PetPage" element={<PetPage />} />
-      <Route path="/TaskPage" element={<TaskPage />} />
+      <Route path="/TaskPage" element={<RequireAuth loginPath="/" component={TaskPage} />} />
+      <Route path="/PetPage" element={<RequireAuth loginPath="/" component={PetPage} />} />
     </Routes>
   );
 }
+
 export default App;
- 
--- packages/client/src/components/login.js
+++ packages/client/src/components/login.js
@@ -3,11 +3,9 @@ import { Button, Modal } from "react-bootstrap";
 import axios from "../util/axiosConfig";
 import { useSignIn } from "react-auth-kit";
 
-
-
 const Login = ({ showModal, handleCloseModal }) => {
   const [data, setData] = useState({ email: "", password: "" });
-const signin = useSignIn()
+  const signin = useSignIn();
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setData((prevData) => ({ ...prevData, [name]: value }));
@@ -15,16 +13,24 @@ const signin = useSignIn()
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    
+
     try {
       const response = await axios.post("/auth/signin", data);
+
+      signin({
+        token:response.data.token,
+        expiresIn:360000,
+        tokenType:"Bearer",
+        authState:{email: data.email}
+      })
+
       const token = response.headers.authorization;
 
       localStorage.setItem("token", token);
-      
+
       console.log("Login successful!");
       console.log("User:", response.data);
-      
+
       handleCloseModal();
     } catch (error) {
       console.error("Login error:", error.message);
@@ -38,8 +44,20 @@ const signin = useSignIn()
       </Modal.Header>
       <Modal.Body>
         <form onSubmit={handleLogin}>
-          <input type="email" name="email" placeholder="Email" value={data.email} onChange={handleInputChange} />
-          <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleInputChange} />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={data.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={data.password}
+            onChange={handleInputChange}
+          />
           <Button type="submit">Login</Button>
         </form>
       </Modal.Body>
