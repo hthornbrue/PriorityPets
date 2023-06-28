9ece4bf141eec3ba15740b3b9e362c54d79a6ea3
Clifford Hunter <clifford.hunter@mykenzie.snhu.edu>
2023-06-27 21:45:52 -0500

    issues with auth

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "react-auth-kit";
+// import { AuthProvider } from "react-auth-kit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider authType={"cookie"} authName={"_auth"} cookieDomain={window.location.hostname} cookieSecure={false}>
+  // <AuthProvider authType={"cookie"} authName={"_auth"} cookieDomain={window.location.hostname} cookieSecure={false}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </AuthProvider>
+  // {/* </AuthProvider> */}
 );
 
 // If you want to start measuring performance in your app, pass a function
