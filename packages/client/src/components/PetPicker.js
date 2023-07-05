c9706af8633d4d0011563ae8b8322bab1d798bd8
Eliot Slavik <115732920+EliotSlavik@users.noreply.github.com>
2023-07-05 09:53:30 -0500

    working on PetGame and PetPicker

--- packages/client/src/components/PetPicker.js
+++ packages/client/src/components/PetPicker.js
@@ -1,58 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./PetPicker.css";
-import axios from "../util/axiosConfig";
-import { Container, Form } from "react-bootstrap";
-import useAuth from "../hooks/useAuth";
+import axios from "axios";
+import { petContext } from "../contexts/petContext";
 
-const imgs = ["/x2/Cat_Down@2x.png", "/x2/Chick_Down@2x.png", "/x2/Fox_Down@2x.png", "/x2/Mouse_Down@2x.png", "/x2/Pig_Down@2x.png", "/x2/Rabbit_Down@2x.png"];
+const imgs = [
+  "/x2/Cat_Down@2x.png",
+  "/x2/Chick_Down@2x.png",
+  "/x2/Fox_Down@2x.png",
+  "/x2/Mouse_Down@2x.png",
+  "/x2/Pig_Down@2x.png",
+  "/x2/Rabbit_Down@2x.png",
+];
 
-const initialData = {
-  name: "",
-  appearance: "",
-  healthLevel: 100,
-};
+const PetPicker = ({ selected, setSelected }) => {
+  const { setPet } = useContext(petContext);
+  const [formData, setFormData] = useState({
+    name: "",
+    appearance: "",
+    healthLevel: 100,
+  });
 
-const PetPicker = ({ selected, setSelected, pet }) => {
-  const [formData, setFormData] = useState(initialData);
-  const auth = useAuth();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(formData);
-    try {
-      const response = await axios.post(`/pets/${selected}`, { user: auth.email, ...formData });
-      setFormData(initialData);
-      console.log("Updated pet:", response.data);
-    } catch (error) {
-      console.log("Error occurred while submitting the form:", error);
-    }
-  };
-
-  const handlePetSelection = (pet) => {
+  const handlePetSelection = async (pet) => {
     setSelected(pet);
-    setFormData({ ...initialData, appearance: pet });
-  };
+    setFormData({ ...formData, appearance: pet });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    try {
+      const response = await axios.post(`/pets/${pet}`, formData);
+      console.log("Updated pet:", response.data);
+      setPet(response.data); 
+    } catch (error) {
+      console.log("Error occurred while updating the pet:", error);
+    }
   };
 
   return (
     <div>
-      <Container className="pet-container">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <div className="pet-card">
-              {imgs.map((pet) => (
-                <img id="image" className={pet === selected ? "selected" : ""} key={pet} src={pet} alt={"pet"} onClick={() => handlePetSelection(pet)} />
-              ))}
-            </div>
-            <Form.Control type="text" placeholder="Pet Name" name="name" value={formData.name} onChange={handleInputChange} />
-            <button type="submit">Submit</button>
-          </Form.Group>
-        </Form>
-      </Container>
+      <div className="pet-card">
+        {imgs.map((pet) => (
+          <img
+            id="image"
+            className={pet === selected ? "selected" : ""}
+            key={pet}
+            src={pet}
+            alt="pet"
+            onClick={() => handlePetSelection(pet)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
